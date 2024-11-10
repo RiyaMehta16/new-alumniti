5a49cb6f89018b7caa5ef15c2965dfb4693bae39
Yogesh Kumar <117355987+1Yogeshh@users.noreply.github.com>
2024-11-10 18:22:17 +0530

    add

--- src/Components/Aimentor/Chatbot.js
+++ src/Components/Aimentor/Chatbot.js
@@ -39,7 +39,7 @@ const Chatbot = () => {
   const sendMessage = async (userMessage) => {
     try {
       const response = await axios.post(
-        'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key',
+        'https://alumniti-app.vercel.app/login',
         {
           contents: [
             {
